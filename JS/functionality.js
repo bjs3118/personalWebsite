d3783597ea4b163be66ecec2a151a18d096e0013
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-10-09 21:41:46 +0100

    about section started

---
+++ JS/functionality.js
@@ -0,0 +1,12 @@
+
+// Opening and closeing navigation bar for mobile devices \\
+ 
+var navLinks = document.getElementById("navLinks");
+
+function showMenue(){
+    navLinks.style.right = "0";
+}
+
+function hideMenue(){
+    navLinks.style.right = "-200px";
+}
